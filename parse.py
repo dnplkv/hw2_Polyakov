0034da2e117839e7cef8f6a3e6693b92ad57ac7d
danny <dannypolyakov95@gmail.com>
2021-03-17 20:10:13 +0200

    parse_branch — file parse.py was added

---
+++ parse.py
@@ -0,0 +1,14 @@
+import re
+
+
+def parse(query: str) -> dict:
+    result = re.findall(r'\W?(\w+)=(\w+)\W?', query)
+    return dict(result)
+
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
